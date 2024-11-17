b89fe77a4e3b07e64668cd529c784e41c3da5fc1
ADRACHE <adrache@etsinf.upv.es>
2024-11-17 19:44:36 +0100

    renombrado json por problemas de libreria, hecho xmlParser

--- convertidores/xmlParser.py
+++ convertidores/xmlParser.py
@@ -1,8 +1,47 @@
 import xml.etree.ElementTree as ET
+import json
+
 # To modify - receiving any file
-file = 'monumentos.xml'
+file = 'datos/monumentos.xml'
 tree = ET.parse(file)
 root = tree.getroot()
 
-for tipoMonumento in root.iter('tipoMonumento'):
-    print(tipoMonumento.atrib)
+with open('datos/CLEdata.json', 'w', encoding='utf-8') as f:
+
+    for monumento in root.iter('monumento'):
+        coords = monumento.find('coordenadas')
+        provincia = 'null'
+        localidad = 'null'
+
+        poblacion = monumento.find('poblacion')
+        if poblacion is not None:
+            localidad = poblacion.find('localidad').text
+            provincia = poblacion.find('provincia').text
+
+        calle = monumento.find('calle')
+        if calle is not None :
+            calle = calle.text
+
+        codpost = monumento.find('codigoPostal')
+        if codpost is not None :
+            codpost = codpost.text
+
+        descripcion = monumento.find('Descripcion')
+        if descripcion is not None :
+            descripcion = descripcion.text
+
+        item = {
+            "name" : monumento.find('nombre').text,
+            "type" : monumento.find('tipoMonumento').text,
+            "address" : calle,
+            "postal code" : codpost,
+            "longitude" : coords.find('longitud').text,
+            "latitude" : coords.find('latitud').text,
+            "description" : descripcion,
+            "localName" : localidad,
+            "provinceName" : provincia
+        }
+        json.dump(item, f, ensure_ascii=False, indent=4)
+
+
+    
